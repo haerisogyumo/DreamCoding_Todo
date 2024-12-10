9e42bd23d816bb2cc8cbba6a225f0fd35e2656ba
진해리 <jinhaeri@jinhaeliui-MacBookPro.local>
2024-12-10 22:15:01 +0900

    feat: 로컬스토리지 연동 완료

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,21 +1,10 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import AddTodo from "../AddTodo/AddTodo";
 import Todo from "../Todo/Todo";
 import styles from "./TodoList.module.css";
 
 export default function TodoList({ filter }) {
-  const [todos, setTodos] = useState([
-    {
-      id: "123",
-      text: "장보기",
-      status: "active",
-    },
-    {
-      id: "124",
-      text: "공부하기",
-      status: "active",
-    },
-  ]);
+  const [todos, setTodos] = useState(() => readTodosFromLocalStorage());
   const handleAdd = (todo) => {
     setTodos([...todos, todo]);
   };
@@ -25,6 +14,10 @@ export default function TodoList({ filter }) {
   const handleDelete = (deleted) => {
     setTodos(todos.filter((t) => t.id !== deleted.id));
   };
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
   const filtered = getFilteredItems(todos, filter);
   return (
     <section className={styles.container}>
@@ -43,6 +36,11 @@ export default function TodoList({ filter }) {
   );
 }
 
+function readTodosFromLocalStorage() {
+  console.log("readTodosFromLocalStorage");
+  const todos = localStorage.getItem("todos");
+  return todos ? JSON.parse(todos) : [];
+}
 
 function getFilteredItems(todos, filter) {
   if (filter === "all") return todos;
