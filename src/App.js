3733d5c97be4f08d2de9a0f73d91d12861d1982c
진해리 <jinhaeri@jinhaeliui-MacBookPro.local>
2024-12-10 21:12:17 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import TodoList from "./components/TodoList/TodoList";
+import Header from "./Header/Header";
+import { useState } from "react";
+
+const filters = ["all", "active", "completed"];
+function App() {
+  const [filter, setFilter] = useState(filters[0]);
+  return (
+    <>
+      <Header filters={filters} filter={filter} onFilterChange={setFilter} />
+      <TodoList filter={filter} />
+    </>
+  );
+}
+
+export default App;
